bfa0c1073f46e48488e4985ac8bf3e4b0097b071
Toni Harzendorf <toni.harzendorf@ufz.de>
2026-05-14 11:25:19 +0200

    remove harpoon

--- .config/nvim/lua/tazend/packer.lua
+++ .config/nvim/lua/tazend/packer.lua
@@ -28,7 +28,6 @@ return require('packer').startup(function(use)
         end,
     }
     use("nvim-treesitter/playground")
-    use("theprimeagen/harpoon")
     use("mbbill/undotree")
     use("tpope/vim-fugitive")
     use("pearofducks/ansible-vim")
